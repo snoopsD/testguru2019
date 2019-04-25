cdd6ad2340891dfe8da88c3ea6eea4bf246196fc
DmitryP <snoops@list.ru>
2019-04-25 09:21:13 +0400

    some fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ApplicationRecord
-  has_many :tests, foreign_key: :creator_id
+  has_many :own_tests, class_name: 'Test', foreign_key: :creator_id
   has_many :finished_tests
   has_many :tests, through: :finished_tests
-  has_many :tests_users
-  has_many :tests, through: :tests_users
 
   def finish_tests(level)
     Test.joins(:finished_tests)
