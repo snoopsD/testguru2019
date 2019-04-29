02d1928fe8e418dff9909c8c0ea74a756476dcb6
DmitryP <snoops@list.ru>
2019-04-29 12:21:47 +0400

    Add routes questions, show, index, create

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,49 @@
+class QuestionsController < ApplicationController
+  before_action :set_question, only: %i[show destroy]
+  before_action :set_test, only: %i[create]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
+
+  def index
+    @questions = Question.all
+  end
+
+  def new    
+    
+  end
+
+  def create
+    @question = @test.questions.create(question_params)
+    
+    render plain: "Вопрос создан"
+  end
+
+  def show    
+    
+  end
+
+  def destroy
+    @question.destroy
+
+    redirect_to :root
+  end
+
+  private
+
+  def set_test
+    @test = Test.find(params[:id])
+  end
+
+  def set_question
+    @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:body)
+  end
+
+  def rescue_with_test_not_found
+    render plain: 'Question not found'
+  end
+  
+end
