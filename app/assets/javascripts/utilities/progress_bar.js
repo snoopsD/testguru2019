1e5a564674a93aedf77802bf4d21366a84ebd309
DmitryP <snoops@list.ru>
2019-05-28 11:59:00 +0400

    Finish lesson with data-attr

---
+++ app/assets/javascripts/utilities/progress_bar.js
@@ -0,0 +1,17 @@
+document.addEventListener('turbolinks:load', function() {
+  var control = document.getElementById('current-test')
+
+  if (control) {
+    var questionId = document.getElementById('current-test').dataset.currentQuestion
+    var questionsCount = document.getElementById('current-test').dataset.questionsCount
+
+    formProgressBar(questionId, questionsCount)
+  }
+})
+
+function formProgressBar(questionId, questionsCount) {
+  var elem = document.getElementById('myBar')
+
+  elem.style.width = (questionId * 100) / questionsCount + "%"
+}
+
--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,9 +1,9 @@
 class Admin::TestsController < Admin::BaseController
   
-  before_action :set_test, only: %i[show edit update destroy start]
+  before_action :set_tests, only: %i[index update_inline]
+  before_action :set_test, only: %i[show edit update destroy start update_inline]
   
   def index
-    @tests = Test.all
   end
 
   def show    
@@ -41,15 +41,23 @@ class Admin::TestsController < Admin::BaseController
     redirect_to admin_tests_path, notice: t('.success')
   end
 
+  def update_inline
+    if @test.update(test_params)
+      redirect_to admin_tests_path
+    else
+      render :index
+    end
+  end
+
   private 
 
   def set_test
     @test = Test.find(params[:id])
   end
-  
-  def set_creator
-  
-  end
+
+  def set_tests
+    @tests = Test.all
+  end  
 
   def test_params
     params.require(:test).permit(:title, :level, :category_id, :creator_id)
